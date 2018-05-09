796f80466c262013e3bf1a1db557e508766289c9
aenrosello <aenrosello+git@gmail.com>
2018-05-08 20:56:25 -0500

    add user section

--- app/users/route.js
+++ app/users/route.js
@@ -2,4 +2,7 @@ import Route from '@ember/routing/route';
 import AdministratorRouteMixin from '../mixins/administrator-route-mixin';
 
 export default Route.extend(AdministratorRouteMixin, {
+  model(){
+    return this.store.findAll('user');
+  }
 });
